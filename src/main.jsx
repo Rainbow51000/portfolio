6ecf299c10420340be2a49ff5b12a21a7b230262
Ruddy Gobillard <ruddy.gobillard@outlook.fr>
2026-02-24 19:40:22 +0100

    Fix: Correction import Tailwind

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from "react-router-dom";
 import Header from "./components/header.jsx";
 import Footer from "./components/footer.jsx";
-import '../public/style.css'
+import './style.css'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
