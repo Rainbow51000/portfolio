e82033c13bc01639a8e5807de671bebdf0702cf3
Ruddy Gobillard <ruddy.gobillard@outlook.fr>
2026-02-24 18:15:49 +0100

    Feat: app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import './components/navbar.jsx'
-import Navbar from "./components/navbar.jsx";
-import Footer from "./components/footer.jsx";
+import { Routes, Route } from "react-router-dom";
+import Accueil from "./pages/accueil.jsx";
+import Parcours from "./pages/parcours.jsx";
+import Projets from "./pages/projets.jsx";
+import Contact from "./pages/contact.jsx";
 
 function App() {
-  return (<></>);
+  return (<>
+  <Routes>
+    <Route path="/" component={Accueil} />
+    <Route path={"/parcours"} component={Parcours} />
+    <Route path={"/projets"} component={Projets} />
+    <Route path={"/contact"} component={Contact} />
+  </Routes>
+  </>);
 }
 
 export default App
