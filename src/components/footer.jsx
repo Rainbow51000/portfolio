ce0465d6321c5b0dccad6239ede51bae1bc5849f
Ruddy Gobillard <ruddy.gobillard@outlook.fr>
2026-02-26 16:00:57 +0100

    Feat: icône dans le footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,9 +1,15 @@
+import githubIcon from '@/assets/github-icon.svg';
+import linkedinIcon from '@/assets/linkedin-icon.svg';
+
 /**
  * Footer with basic links and info
  * @returns {React.JSX.Element}
  */
 export default function Footer() {
     return (
-        <footer className={"footer"}>FOOTER</footer>
+        <footer className="w-full bg-black ">
+            <img src={githubIcon}  alt={"Icône GitHub"}/>
+            <img src={linkedinIcon}  alt={"Icône LinkedIn"}/>
+        </footer>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,15 @@ import Header from "./components/header.jsx";
 import Footer from "./components/footer.jsx";
 import './style.css'
 
+/**
+ * Page content will appear inside the <main> tag.
+ */
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Header />
-    <App />
+    <main>
+      <App />
+    </main>
     <Footer />
   </BrowserRouter>,
 )
