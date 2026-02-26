c76df556974fa2da32cf2792f4ac47fc5dfb1027
Ruddy Gobillard <ruddy.gobillard@outlook.fr>
2026-02-26 16:00:48 +0100

    Chore: doc app()

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,20 @@ import Parcours from "./pages/parcours.jsx";
 import Projets from "./pages/projets.jsx";
 import Contact from "./pages/contact.jsx";
 
+/**
+ * Main function, appears on every pages!
+ * Content of a page will appear on top of the <Routes> tag.
+ * Do not edit it as the "index", edit only if you want content to appear everywhere.
+ * @returns {React.JSX.Element} Page displayed
+ */
 function App() {
   return (<>
-  <Routes>
-    <Route path="/" element={<Accueil />} />
-    <Route path={"/parcours"} element={<Parcours />} />
-    <Route path={"/projets"} element={<Projets />} />
-    <Route path={"/contact"} element={<Contact />} />
-  </Routes>
+    <Routes>
+      <Route path="/" element={<Accueil />} />
+      <Route path={"/parcours"} element={<Parcours />} />
+      <Route path={"/projets"} element={<Projets />} />
+      <Route path={"/contact"} element={<Contact />} />
+    </Routes>
   </>);
 }
 
